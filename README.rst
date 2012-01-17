48ba115b56d3a7c53bd914f0ddea432070d9b25e
Brandon Dixon <brandon@9bplus.com>
2012-01-16 21:10:52 -0500

    added in harvert which uses SQLite instead of a txt file for tracking

--- README.rst
+++ README.rst
@@ -2,10 +2,14 @@ Purpose
 =======
 Search through Pastebin.com for interesting pastes based on keywords
 
+Files
+=====
+* snatch.py - outputs to the screen and uses the tracker .txt file to monitor previously seen URLs
+* harvest.py - stores data inside of SQLite instead of a text file
+
 Plans
 =====
 * Create easy way to add in keywords
-* Track in a more efficient manner
 * Replace nesting with generators
 * Add in emailer
 * Daemonize!
---
+++ harvest.py
@@ -0,0 +1,48 @@
+import httplib2
+import re
+import sqlite3 as sql
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+
+#declare
+db = "urls.db"
+base_url = "http://pastebin.com"
+keywords = ['password','hack']
+
+try:
+	con = sql.connect(db)
+except:
+	print "Could not connect to " + db
+
+def clean(val):
+    if type(val) is not str: val = str(val)
+    val = re.sub(r'<.*?>', '', val) #remove tags
+    return val.strip() #remove leading & trailing whitespace
+
+def save_url(url):
+	cur = con.cursor()
+	cur.execute("insert into urls(url) values(?)",[url])
+	con.commit() 
+
+def check_url(url):
+	cur = con.cursor()
+	cur.execute("select * from urls where url=?",[url])
+	result = cur.fetchone()
+	if result:
+		return False
+	else:
+		return True
+
+http = httplib2.Http()
+status, response = http.request('http://pastebin.com/archive')
+product = SoupStrainer("td",{"class":"icon"})
+soup = BeautifulSoup(response,parseOnlyThese=product)
+for link in soup.findAll("a"):
+	app = link["href"]
+	if check_url(app):
+		save_url(app)
+		tmper = base_url + app
+		status, response = http.request(tmper)
+		feast = BeautifulSoup(response,parseOnlyThese=SoupStrainer("textarea")) #appears to only have one textarea
+		m = re.search('|'.join(keywords),str(feast))
+		if m:
+			print tmper + " matched " +  m.group()
