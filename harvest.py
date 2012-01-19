30001c95c3a39ccbafec379e0e60714fc4979dad
Clemens-O. Hoppe <coh@co-hoppe.net>
2012-01-19 20:02:59 +0100

    now PEP8 clean

--- harvest.py
+++ harvest.py
@@ -4,10 +4,13 @@ import re
 import sqlite3 as sql
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 
+
 #declare
 db = "urls.db"
 base_url = "http://pastebin.com"
-keywords = ['password','hack']
+keywords = ['password',
+            'hack',
+           ]
 
 
 try:
@@ -17,20 +20,21 @@ except:
 
 
 def clean(val):
-    if type(val) is not str: val = str(val)
-    val = re.sub(r'<.*?>', '', val) #remove tags
-    return val.strip() #remove leading & trailing whitespace
+    if type(val) is not str:
+        val = str(val)
+    val = re.sub(r'<.*?>', '', val)  # remove tags
+    return val.strip()  # remove leading & trailing whitespace
 
 
-def save_url(url,matcher):
+def save_url(url, matcher):
     cur = con.cursor()
-    cur.execute("insert into urls(url,matcher) values(?,?)",[url,matcher])
-    con.commit() 
+    cur.execute("insert into urls(url,matcher) values(?,?)", [url, matcher])
+    con.commit()
 
 
 def check_url(url):
     cur = con.cursor()
-    cur.execute("select * from urls where url=?",[url])
+    cur.execute("select * from urls where url=?", [url])
     result = cur.fetchone()
     if result:
         return False
@@ -40,21 +44,23 @@ def check_url(url):
 
 def fetch():
     http = httplib2.Http()
-    status, response = http.request('http://pastebin.com/archive')	
-    product = SoupStrainer("td",{"class":"icon"})
-    soup = BeautifulSoup(response,parseOnlyThese=product)
+    status, response = http.request('http://pastebin.com/archive')
+    product = SoupStrainer("td", {"class": "icon"})
+    soup = BeautifulSoup(response, parseOnlyThese=product)
     for link in soup.findAll("a"):
         app = link["href"]
         if check_url(app):
             tmper = base_url + app
             status, response = http.request(tmper)
-            feast = BeautifulSoup(response,parseOnlyThese=SoupStrainer("textarea")) #appears to only have one textarea
-            m = re.search('|'.join(keywords),str(feast))
+            # appears to only have one textarea
+            feast = BeautifulSoup(response,
+                                  parseOnlyThese=SoupStrainer("textarea"))
+            m = re.search('|'.join(keywords), str(feast))
             if m:
-                print tmper + " matched " +  m.group()
-                save_url(app,str(m.group()))
+                print tmper + " matched " + m.group()
+                save_url(app, str(m.group()))
             else:
-                save_url(app,"")
+                save_url(app, "")
 
 
 if __name__ == "__main__":
--- snatch.py
+++ snatch.py
@@ -5,26 +5,30 @@ from BeautifulSoup import BeautifulSoup, SoupStrainer
 
 #declare
 base_url = "http://pastebin.com"
-keywords = ['password','hack']
+keywords = ['password',
+            'hack',
+           ]
 
 
 def clean(val):
-    if type(val) is not str: val = str(val)
-    val = re.sub(r'<.*?>', '', val) #remove tags
-    return val.strip() #remove leading & trailing whitespace
+    if type(val) is not str:
+        val = str(val)
+    val = re.sub(r'<.*?>', '', val)  # remove tags
+    return val.strip()  # remove leading & trailing whitespace
 
 
 def save_url(url):
-    tracker = open("tracker.txt","a")
+    tracker = open("tracker.txt", "a")
     tracker.write(str(url) + "\n")
     tracker.close()
 
 
 def check_url(url):
-    tracker = open("tracker.txt","r+")
+    tracker = open("tracker.txt", "r+")
     lines = tracker.readlines()
+
     for line in lines:
-        m = re.search(url,line)
+        m = re.search(url, line)
         if m:
             return False
     return True
@@ -32,8 +36,8 @@ def check_url(url):
 
 http = httplib2.Http()
 status, response = http.request('http://pastebin.com/archive')
-product = SoupStrainer("td",{"class":"icon"})
-soup = BeautifulSoup(response,parseOnlyThese=product)
+product = SoupStrainer("td", {"class": "icon"})
+soup = BeautifulSoup(response, parseOnlyThese=product)
 
 for link in soup.findAll("a"):
     app = link["href"]
@@ -42,8 +46,10 @@ for link in soup.findAll("a"):
         save_url(app)
         tmper = base_url + app
         status, response = http.request(tmper)
-        feast = BeautifulSoup(response,parseOnlyThese=SoupStrainer("textarea")) #appears to only have one textarea
-        m = re.search('|'.join(keywords),str(feast))
+        # appears to only have one textarea
+        feast = BeautifulSoup(response,
+                              parseOnlyThese=SoupStrainer("textarea"))
+        m = re.search('|'.join(keywords), str(feast))
 
         if m:
-            print tmper + " matched " +  m.group()
+            print tmper + " matched " + m.group()
