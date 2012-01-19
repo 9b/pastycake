6bb78a99dd995a29d8e579cd3a238c048d0eb1a9
coh <coh@laptop3.(none)>
2012-01-19 15:09:58 +0100

    PEP 8 fixes

--- harvest.py
+++ harvest.py
@@ -9,49 +9,55 @@ db = "urls.db"
 base_url = "http://pastebin.com"
 keywords = ['password','hack']
 
+
 try:
-	con = sql.connect(db)
+    con = sql.connect(db)
 except:
-	print "Could not connect to " + db
+    print "Could not connect to " + db
+
 
 def clean(val):
     if type(val) is not str: val = str(val)
     val = re.sub(r'<.*?>', '', val) #remove tags
     return val.strip() #remove leading & trailing whitespace
 
+
 def save_url(url,matcher):
-	cur = con.cursor()
-	cur.execute("insert into urls(url,matcher) values(?,?)",[url,matcher])
-	con.commit() 
+    cur = con.cursor()
+    cur.execute("insert into urls(url,matcher) values(?,?)",[url,matcher])
+    con.commit() 
+
 
 def check_url(url):
-	cur = con.cursor()
-	cur.execute("select * from urls where url=?",[url])
-	result = cur.fetchone()
-	if result:
-		return False
-	else:
-		return True
+    cur = con.cursor()
+    cur.execute("select * from urls where url=?",[url])
+    result = cur.fetchone()
+    if result:
+        return False
+    else:
+        return True
+
 
 def fetch():
-	http = httplib2.Http()
-	status, response = http.request('http://pastebin.com/archive')	
-	product = SoupStrainer("td",{"class":"icon"})
-	soup = BeautifulSoup(response,parseOnlyThese=product)
-	for link in soup.findAll("a"):
-		app = link["href"]
-		if check_url(app):
-			tmper = base_url + app
-			status, response = http.request(tmper)
-			feast = BeautifulSoup(response,parseOnlyThese=SoupStrainer("textarea")) #appears to only have one textarea
-			m = re.search('|'.join(keywords),str(feast))
-			if m:
-				print tmper + " matched " +  m.group()
-				save_url(app,str(m.group()))
-			else:
-				save_url(app,"")
+    http = httplib2.Http()
+    status, response = http.request('http://pastebin.com/archive')	
+    product = SoupStrainer("td",{"class":"icon"})
+    soup = BeautifulSoup(response,parseOnlyThese=product)
+    for link in soup.findAll("a"):
+        app = link["href"]
+        if check_url(app):
+            tmper = base_url + app
+            status, response = http.request(tmper)
+            feast = BeautifulSoup(response,parseOnlyThese=SoupStrainer("textarea")) #appears to only have one textarea
+            m = re.search('|'.join(keywords),str(feast))
+            if m:
+                print tmper + " matched " +  m.group()
+                save_url(app,str(m.group()))
+            else:
+                save_url(app,"")
+
 
 if __name__ == "__main__":
-	while(1):
-		fetch()
-		time.sleep(5)
+    while(1):
+        fetch()
+        time.sleep(5)
