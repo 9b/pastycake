1e6b0293d9b0ccfd0c27a86f68167ef2c4b6d520
Clemens-O. Hoppe <coh@co-hoppe.net>
2012-01-23 08:12:55 +0100

    simplified the mailer and made the sendername modifiable. pep8 fixes to both mailer and pastebin_source

--- pastycake/mailer.py
+++ pastycake/mailer.py
@@ -1,47 +1,44 @@
-import sys
-import socket
 import smtplib
-import string
+import socket
+import sys
+
+from email.mime.text import MimeText
+
 from pastebin_source import PastebinSource
 
-class Mailer():
-    def __init__(self,recv):
-        self._recv = recv
 
-    def get_paste_text(self,url):
+class Mailer(object):
+    def __init__(self, recv, sender=None):
+        self._recv = recv
+        self._sender = sender
+
+    def get_paste_text(self, url):
         pb = PastebinSource()
         status, data = pb.get_paste(url)
         if data == '':
             data = "Failed to pull paste data"
         return data
 
-    def sendmail(self,url,matcher):
-        sender = "pastycake@" + socket.gethostname()
+    def sendmail(self, url, matcher):
+        sender = self._sender or "pastycake@" + socket.gethostname()
         recv = self._recv
         subject = "[PastyCake] " + url + " matched " + matcher
         text = self.get_paste_text(url)
 
         try:
-            body = string.join((
-                "From: %s" % sender,
-                "To: %s" % recv,
-                "Subject: %s" % subject,
-                "",
-                str(text)
-            ), "\r\n")
+            text = str(text)
         except:
-            body = string.join((
-                "From: %s" % sender,
-                "To: %s" % recv,
-                "Subject: %s" % subject,
-                "",
-                str("Failed to handle non-ascii code")
-            ), "\r\n")
+            text = ''
+
+        msg = MimeText(text)
+        msg['subject'] = subject
+        msg['From'] = sender
+        msg['To'] = recv
 
         server = smtplib.SMTP("localhost")
         try:
-            server.sendmail(sender, recv, body)
+            server.sendmail(sender, recv, msg.as_string())
             server.close()
-        except Exception, e:
-            print >> sys.stderr, "Unable to send email. Error: %s" % str(e)
+        except smtplib.SMTPException as e:
+            print >> sys.stderr, "Unable to send email. Error: %s" % e
             sys.exit(1)
