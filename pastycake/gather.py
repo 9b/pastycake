394c81736ccb44c24291c17b2ac6f8830fe4bf4d
Clemens-O. Hoppe <coh@co-hoppe.net>
2012-02-14 15:17:31 +0100

    implement the basics for 9b/pastycake#23

--- pastycake/gather.py
+++ pastycake/gather.py
@@ -8,32 +8,42 @@ import louie as L
 from pastycake.config import Config
 
 
+def _fetch_one(generator, path, keywords, storage, store_match):
+    status, data = generator.get_paste(path)
+
+    #if 5xx or 4xx
+    if status['status'][0] in ('4', '5'):
+        #TODO better handling of timeouts
+        print >> sys.stderr, "%s: %s. skipping" % (path,
+                                                    status['status'])
+        return
+
+    full_url = generator.full_url(path)
+    match = None
+
+    for kw in keywords:
+        if hasattr(kw, '__call__'):
+            match = kw(data)
+        else:
+            match = re.search(kw, data)
+
+        storage.save_url(full_url,
+                            match.group() if match and store_match else None)
+        if match:
+            L.send('match', generator, storage, match=match.group(),
+                    url=full_url, data=data)
+            # stop after the first match
+            return
+
+
 #def fetch(storage, sources, keywords, store_match):
 def fetch(conf_obj, store_match=True):
-    search_re = re.compile('|'.join(conf_obj['keywords']))
+    keywords = conf_obj['keywords']
     storage = conf_obj['backend']
 
     for src in conf_obj['sources']:
         for generator, path in src.new_urls(storage):
-            status, data = generator.get_paste(path)
-
-            #if 5xx or 4xx
-            if status['status'][0] in ('4', '5'):
-                #TODO better handling of timeouts
-                print >> sys.stderr, "%s: %s. skipping" % (path,
-                                                           status['status'])
-                continue
-
-            full_url = generator.full_url(path)
-
-            match = search_re.search(str(data))
-
-            storage.save_url(full_url,
-                             match.group() if match and store_match else None)
-
-            if match:
-                L.send('match', generator, storage, match=match.group(),
-                       url=full_url, data=data)
+            _fetch_one(generator, path, keywords, storage, store_match)
 
 
 def main(args=None):
